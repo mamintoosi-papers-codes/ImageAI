304caa1ca8e40a92232ed55738261c629e9031fb
Mahmood Amintoosi <mamintoosi@yahoo.com>
2021-08-21 21:00:50 +0430

    Update convert_yolo3_to_keras.py

--- utils/convert_yolo3_to_keras.py
+++ utils/convert_yolo3_to_keras.py
@@ -132,7 +132,7 @@ class WeightReader:
 		for i in range(106):
 			try:
 				conv_layer = model.get_layer('conv_' + str(i))
-				# print("loading weights of convolution #" + str(i))
+				print("loading weights of convolution #" + str(i))
 				if i not in [81, 93, 105]:
 					norm_layer = model.get_layer('bnorm_' + str(i))
 					size = np.prod(norm_layer.get_weights()[0].shape)
@@ -153,7 +153,7 @@ class WeightReader:
 					kernel = kernel.transpose([2,3,1,0])
 					conv_layer.set_weights([kernel])
 			except ValueError:
-				# print("no convolution #" + str(i))
+				print("no convolution #" + str(i))
  
 	def reset(self):
 		self.offset = 0
